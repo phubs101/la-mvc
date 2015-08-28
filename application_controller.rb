40c75d31923b55f940a2b7d26ebeb3a0d50422c6
phubs101 <izzy8awsome@gmail.com>
2015-08-28 17:28:53 +0000

    anything

--- application_controller.rb
+++ application_controller.rb
@@ -1,9 +1,24 @@
 require 'bundler'
+require_relative 'models/model.rb'
 Bundler.require
 
 class MyApp < Sinatra::Base
 
+
   get '/' do
     erb :index
   end
+
+  get '/index2.erb' do
+    erb :index2
+  end
+
+  post '/index2.erb' do
+    puts params
+    present = Perfect_present.new
+    @my_present=present.result(params[:age],params[:gender],params[:relationship],params[:price],params[:categories])
+    puts @my_present
+    erb :anything
+  end
+
 end
